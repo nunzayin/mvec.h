305245b4eb90adf3a046b1b73b0a789df8214c8b
nz <64735893+nunzayin@users.noreply.github.com>
2025-10-01 09:47:14 +0300

    Added more documentation and changed license

--- include/mvec.h
+++ include/mvec.h
@@ -1,36 +1,36 @@
 // mvec.h - Monolithic vector
+
+// MIT License
 //
-// This is free and unencumbered software released into the public domain.
+// Copyright (c) 2025 nunzayin
 //
-// Anyone is free to copy, modify, publish, use, compile, sell, or
-// distribute this software, either in source code form or as a compiled
-// binary, for any purpose, commercial or non-commercial, and by any
-// means.
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
 //
-// In jurisdictions that recognize copyright laws, the author or authors
-// of this software dedicate any and all copyright interest in the
-// software to the public domain. We make this dedication for the benefit
-// of the public at large and to the detriment of our heirs and
-// successors. We intend this dedication to be an overt act of
-// relinquishment in perpetuity of all present and future rights to this
-// software under copyright law.
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
 //
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-// IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-// OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-// OTHER DEALINGS IN THE SOFTWARE.
-//
-// For more information, please refer to <https://unlicense.org>
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
 #ifndef MVEC_H
 #define MVEC_H
 
 #include <stddef.h> // size_t, ptrdiff_t
 
+// You can use void if you care to be straightforward
 typedef void mvec_t;
+// Or this one if you care to be informative
+#define mvec_(T) mvec_t
 
 // Size of the vector header
 static const size_t MVEC_HEADER_SIZE = 2 * sizeof(size_t);
@@ -123,14 +123,41 @@ static inline void mvec_ptrs(
 }
 
 // Returns physical size of a vector, i.e. number of bytes currently allocated
-//
+// for the whole data structure and its elements.
+// Parameters:
+//  @ mvec - pointer to the monolithic vector
+//  @ element_size - size of a single element in bytes
+// Return value:
+//  Physical size of a vector, i.e. number of bytes currently allocated
+//  for the whole data structure and its elements.
+// UB conditions:
+//  @ mvec == NULL or address of not a valid vector
+//  @ passing the element size not equal to the one used to (re)allocate this
+//  vector (will return a misleading value not corresponding to real size)
 static inline size_t mvec_size(mvec_t* mvec, size_t element_size) {
     return MVEC_HEADER_SIZE + *mvec_capacity(mvec) * element_size;
 }
 
+// Define MVEC_IMPLEMENTATION before #include'ing this library to include the
+// implementation of given functions to the current translation unit. Once
+// you've made that, all the translation units to be linked with this one may
+// not include the implementation (but still need to include the header itself
+// though). Common practice for such cases is just to create a separate
+// translation unit (let's call it "mvec.c") and leave there only these
+// directives:
+//  #define MVEC_IMPLEMENTATION
+//  #include "mvec.h"
+//  (EOF)
 #ifdef MVEC_IMPLEMENTATION
 #undef MVEC_IMPLEMENTATION
 
+// You can use custom allocator's malloc() and realloc() functions by defining
+// the corresponding macros. Note that custom functions should have the same
+// semantics and argument sets as of the ones provided by <stdlib.h>. For each
+// allocating function, if its custom version is not provided, <stdlib.h> is
+// included in the translation unit and its corresponding function is used.
+// That means partial setting of custom allocating functions is technically
+// possible, though not recommended (will likely lead to UB).
 #if !defined(MVEC_MALLOC_FUNCTION) || !defined(MVEC_REALLOC_FUNCTION)
 #include <stdlib.h> // malloc, realloc
 #ifndef MVEC_MALLOC_FUNCTION
@@ -141,7 +168,15 @@ static inline size_t mvec_size(mvec_t* mvec, size_t element_size) {
 #endif // !MVEC_REALLOC_FUNCTION
 #endif // !defined(MVEC_MALLOC_FUNCTION) || !defined(MVEC_REALLOC_FUNCTION)
 
+// You can use custom memmove() implementation by defining the corresponding
+// macros. Note that custom function should have the same semantic and argument
+// set as of the one provided by <string.h>. If the custom version is not
+// provided, <string.h> is included in the translation unit and its
+// corresponding function is used.
+#ifndef MVEC_MEMMOVE_FUNCTION
 #include <string.h> // memmove
+#define MVEC_MEMMOVE_FUNCTION memmove
+#endif // !MVEC_MEMMOVE_FUNCTION
 
 // Allocates memory for the vector and initializes its header with given
 // capacity and zero length. The rest of the memory remains uninitialized.
@@ -219,7 +254,7 @@ int mvec_resize(mvec_t** mvec_ptr, size_t new_capacity, size_t element_size) {
 //  @ index - index of an element to start from
 //  @ offset - the offset to shift the vector with (amount of elements, not
 //  bytes)
-//  @ element_size - size of a single element
+//  @ element_size - size of a single element in bytes
 // Return value: (none)
 // UB conditions:
 //  @ mvec == NULL or address of not a valid vector
@@ -239,13 +274,16 @@ void mvec_shift(
 {
     char* mv_data = mvec_data(mvec);
     size_t* mv_len = mvec_length(mvec);
-    memmove(
+    MVEC_MEMMOVE_FUNCTION(
             mv_data + (index + offset) * element_size,
             mv_data + index * element_size,
             (*mv_len - index) * element_size
            );
     *mv_len += offset;
 }
+
+#undef MVEC_MALLOC_FUNCTION
+#undef MVEC_REALLOC_FUNCTION
 
 #endif // MVEC_IMPLEMENTATION
 #endif // !MVEC_H
